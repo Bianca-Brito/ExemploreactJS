9d0acb7e48d6c2b1f3a1ded3cd8bb1655bb0674e
Bianca-Brito <biancabvalp@gmail.com>
2025-06-09 20:49:19 -0300

    atualizando

--- lampada-on-off/pages/index.js
+++ lampada-on-off/pages/index.js
@@ -1,18 +1,52 @@
+import { Html, Head, Main, NextScript } from "next/document"
+import { useState } from "react";
+let clickCount = 0;
+let isQueimada = false;
+ 
+function On() {
+  const lampada = document.querySelector("img");
+  lampada.src = "/assets/on.png";
+  handleClick();
+}
+function Off() {
+  const lampada = document.querySelector("img");
+  lampada.src = "/assets/off.png";
+  handleClick();
+}
+ 
+ 
+function queimado() {
+  const lampada = document.querySelector("img");
+  lampada.src = "/assets/Lâmpada.png";
+ 
+}
+function handleClick() {
+  clickCount++;
+  if (clickCount >= 10 && !isQueimada) {
+    queimado();
+    isQueimada = true;
+  } else if (isQueimada) {
+    alert("A lâmpada já está queimada!");
+    queimado();
+  } else {
+   
+  }
+}
 export default function Home() {
   return (
-   <div class="container-fluid">
-    <div className="row">
-      <div className="col-md-4 offset-md-4">
-        <div className="text-center">
-        <img src="/assets/off.png" />
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-4 offset-md-4">
+          <h1 className="text-center">Lâmpada On/Off</h1>
+          <div className="text-center">
+            <img src="/assets/off.png" alt="Lâmpada" className="img-fluid " style={{ maxWidth: "300px", height: "auto" }} />
+          </div>
+          <div className="text-center">
+            <button className="btn btn-light" onClick={On}>Ligar</button>
+            <button className="btn btn-dark" onClick={Off}>Desligar</button>
+          </div>
         </div>
-        <h1 className="text-center">Lampâda On/Off</h1>
-        <div className="text-center">
-        <button className="btn btn-light">Ligar</button>
-        <button className="btn btn-dark">Desligar</button>
-       </div>
       </div>
     </div>
-   </div>
   );
 }
